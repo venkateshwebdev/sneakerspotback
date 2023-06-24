1b9693fa1a8db6df8484a06848eea5ad8d95814e
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-06-24 19:01:52 +0530

    inital commit

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,8 +1,6 @@
 const Product = require("../models/productsSchema")
 const axios = require('axios')
 exports.addProducts = async(req,res)=>{
-    // const response = await axios.get('https://www.shopstyle.com/api/v2/products?abbreviatedCategoryHistogram=true&allowParamScriptScore=true&autodrill=0&b=puma&cat=mens-sneakers&device=desktop&expandedPriceTitle=true&generatedSnippet=false&includeProducts=true&includeSavedQueryId=true&includeSponsoredProducts=true&limit=40&locales=all&max-price=500&maxNumFilters=1000&min-price=50&offset=40&onModel=retailer&pca=true&pid=shopstyle&prevCat=men&productScore=LessPopularityHighEPC&rankFactors=epc%2Cpop1%2Cclk&rankMultipliers=0.324%2C0.0027%2C9.0&segment=contemporary&smartSearch=0&t=new&url=%2Fbrowse%2Fmens-sneakers%2Fpuma&view=angular');
-    // const data = response.data;
     data.products.map(async(e)=>await Product.create({
         title:e.name,
         price:e.price*80,
@@ -28,6 +26,13 @@ exports.getAbout = async(req,res) => {
 }
 exports.getProducts = async(req,res)=>{
     const data = await Product.find()
+    res.json({
+        data:data
+    })
+}
+exports.getProduct = async(req,res)=>{
+    const param = req.params.id;
+    const data = await Product.findOne({_id:param})
     res.json({
         data:data
     })
--- router/productsRouter.js
+++ router/productsRouter.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const {addProducts,getProducts,getAbout} = require("../controller/productsController")
+const {addProducts,getProducts,getAbout,getProduct} = require("../controller/productsController")
 const router = express.Router()
 
 
 router.route("/").post(addProducts).get(getProducts)
 router.route("/getabout").get(getAbout)
+router.route("/:id").get(getProduct)
 module.exports = router
